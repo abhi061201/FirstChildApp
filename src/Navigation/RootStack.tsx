e9e324e47d821d73ce0b4edf6b7372ad9c188ef1
Abhishek Gupta <ag742757@gmail.com>
2025-08-31 02:31:43 +0530

    added splash screen

--- src/Navigation/RootStack.tsx
+++ src/Navigation/RootStack.tsx
@@ -1,12 +1,18 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ChildScreen1 from '../Screens/ChildScreen1';
 import ChildScreen2 from '../Screens/ChildScreen2';
+import SplashScreen from '../Screens/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 
 function RootStack({ initialProps }: { initialProps: any }) {
   return (
-    <Stack.Navigator initialRouteName="ChildScreen1">
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
+        name="Splash"
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen
         name="ChildScreen1"
         component={ChildScreen1}
--- src/Screens/ChildScreen1.tsx
+++ src/Screens/ChildScreen1.tsx
@@ -4,7 +4,8 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 
 const ChildScreen1 = () => {
   const route = useRoute();
-  const params = route.params
+  const params = route.params?.nativeParams;
+  console.log('params native', JSON.stringify(params));
   const navigation = useNavigation();
   const handlePress = useCallback(() => {
     navigation.navigate('ChildScreen2');
@@ -18,8 +19,28 @@ const ChildScreen1 = () => {
         gap: 20,
       }}
     >
-      <Text>ChildApp</Text>
-      <Button onPress={handlePress} title="Go to screen 2" color={params?.color}/>
+      <Text style={{ fontSize: 24, fontWeight: '800', textAlign: 'center' }}>
+        Mini App {params?.index}
+      </Text>
+      <Text style={{ fontSize: 18, fontWeight: '500', textAlign: 'center' }}>
+        Hi there, this message is for you from superApp
+      </Text>
+      <Text
+        style={{
+          fontSize: 18,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          color: 'teal',
+        }}
+      >
+        {params?.message}
+      </Text>
+
+      <Button
+        onPress={handlePress}
+        title="Go to screen 2"
+        color={params?.color}
+      />
     </View>
   );
 };
